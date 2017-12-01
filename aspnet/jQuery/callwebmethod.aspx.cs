15d9f74e4cae99ca43f3c66ebadd68afdd1987db
ua09108 <ua09108@hotmail.com>
2017-12-01 12:06:56 +0800

    new commit.

---
+++ aspnet/jQuery/callwebmethod.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aspnet.jQuery
+{
+    public partial class callwebmethod : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [System.Web.Services.WebMethod]
+        public static string GetCurrentTime(string name)
+        {
+            return "Hello " + name + Environment.NewLine + "The Current Time is: "
+                + DateTime.Now.ToString();
+        }
+    }
+}
